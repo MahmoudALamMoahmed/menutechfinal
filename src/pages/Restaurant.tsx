b586f63f83490169d583251d69ed102d89d72aa4
MahmoudALamMohamed <mahmoudalm2019@gmail.com>
2025-07-31 03:09:35 +0300

    bugs

--- src/pages/Restaurant.tsx
+++ src/pages/Restaurant.tsx
@@ -454,7 +454,7 @@ ${orderText}
                   className={`p-3 border rounded-md transition ${
                     viewType === "grid"
                       ? "bg-primary text-white border-black"
-                      : "bg-white text-gray-300 border-black"
+                      : "bg-white text-gray-500 border-black"
                   }`}
                 >
                   <LayoutGrid className="w-5 h-5" />
@@ -464,7 +464,7 @@ ${orderText}
                   className={`p-3 border rounded-md transition ${
                     viewType === "list"
                       ? "bg-primary text-white border-black"
-                      : "bg-white text-gray-300 border-black"
+                      : "bg-white text-gray-700 border-black"
                   }`}
                 >
                   <List className="w-5 h-5" />
